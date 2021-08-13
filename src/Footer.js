f8b4ab74fa6790ca54f1672b184f474673f1e357
Patrick Ongeche <daltonongeche@gmail.com>
2021-08-13 09:11:40 +0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2021 The Vlage! rights reserved</p>
+            {/* <p>Privacy · Terms · S Details</p> */}
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import vlage from './assets/img/vlage.png';
+
+function Header() {
+    return (
+        <div className='header'>
+            <Link to='/'>
+                <img className="photo"
+                    className="header__icon"
+                    src={vlage}
+                    alt="THE VLAGE"
+                />
+            </Link>
+           
+            <div className='header__center'>
+                <input type="text" />
+                <SearchIcon />
+            </div>
+
+            <div className='header__right'>
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
+export default Header
